a77b3cc592fa94802da9eb4ab3fad284eb4abcd5
Miguel Canhola <miguelcanhola@hotmail.com>
2016-02-24 09:25:04 +0000

    Form3

--- jogo_assassino/jogo_assassino/Form3.Designer.cs
+++ jogo_assassino/jogo_assassino/Form3.Designer.cs
@@ -28,19 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.Lab_Player1 = new System.Windows.Forms.Label();
+            this.Lab_Player2 = new System.Windows.Forms.Label();
+            this.Lab_Player3 = new System.Windows.Forms.Label();
+            this.Lab_Player4 = new System.Windows.Forms.Label();
+            this.Lab_Player5 = new System.Windows.Forms.Label();
+            this.tex_Player1 = new System.Windows.Forms.TextBox();
+            this.tex_Player2 = new System.Windows.Forms.TextBox();
+            this.tex_Player3 = new System.Windows.Forms.TextBox();
+            this.tex_Player4 = new System.Windows.Forms.TextBox();
+            this.tex_Player5 = new System.Windows.Forms.TextBox();
+            this.List_Classes = new System.Windows.Forms.ListBox();
+            this.Lab_Classe = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Seguinte = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Lab_Player1
+            // 
+            this.Lab_Player1.AutoSize = true;
+            this.Lab_Player1.Location = new System.Drawing.Point(12, 48);
+            this.Lab_Player1.Name = "Lab_Player1";
+            this.Lab_Player1.Size = new System.Drawing.Size(42, 13);
+            this.Lab_Player1.TabIndex = 1;
+            this.Lab_Player1.Text = "Player1";
+            // 
+            // Lab_Player2
+            // 
+            this.Lab_Player2.AutoSize = true;
+            this.Lab_Player2.Location = new System.Drawing.Point(12, 138);
+            this.Lab_Player2.Name = "Lab_Player2";
+            this.Lab_Player2.Size = new System.Drawing.Size(42, 13);
+            this.Lab_Player2.TabIndex = 2;
+            this.Lab_Player2.Text = "Player2";
+            // 
+            // Lab_Player3
+            // 
+            this.Lab_Player3.AutoSize = true;
+            this.Lab_Player3.Location = new System.Drawing.Point(12, 222);
+            this.Lab_Player3.Name = "Lab_Player3";
+            this.Lab_Player3.Size = new System.Drawing.Size(42, 13);
+            this.Lab_Player3.TabIndex = 3;
+            this.Lab_Player3.Text = "Player3";
+            // 
+            // Lab_Player4
+            // 
+            this.Lab_Player4.AutoSize = true;
+            this.Lab_Player4.Location = new System.Drawing.Point(12, 313);
+            this.Lab_Player4.Name = "Lab_Player4";
+            this.Lab_Player4.Size = new System.Drawing.Size(42, 13);
+            this.Lab_Player4.TabIndex = 4;
+            this.Lab_Player4.Text = "Player4";
+            // 
+            // Lab_Player5
+            // 
+            this.Lab_Player5.AutoSize = true;
+            this.Lab_Player5.Location = new System.Drawing.Point(12, 400);
+            this.Lab_Player5.Name = "Lab_Player5";
+            this.Lab_Player5.Size = new System.Drawing.Size(42, 13);
+            this.Lab_Player5.TabIndex = 5;
+            this.Lab_Player5.Text = "Player5";
+            // 
+            // tex_Player1
+            // 
+            this.tex_Player1.Location = new System.Drawing.Point(12, 64);
+            this.tex_Player1.Name = "tex_Player1";
+            this.tex_Player1.Size = new System.Drawing.Size(100, 20);
+            this.tex_Player1.TabIndex = 6;
+            // 
+            // tex_Player2
+            // 
+            this.tex_Player2.Location = new System.Drawing.Point(12, 154);
+            this.tex_Player2.Name = "tex_Player2";
+            this.tex_Player2.Size = new System.Drawing.Size(100, 20);
+            this.tex_Player2.TabIndex = 7;
+            // 
+            // tex_Player3
+            // 
+            this.tex_Player3.Location = new System.Drawing.Point(12, 238);
+            this.tex_Player3.Name = "tex_Player3";
+            this.tex_Player3.Size = new System.Drawing.Size(112, 20);
+            this.tex_Player3.TabIndex = 8;
+            // 
+            // tex_Player4
+            // 
+            this.tex_Player4.Location = new System.Drawing.Point(12, 329);
+            this.tex_Player4.Name = "tex_Player4";
+            this.tex_Player4.Size = new System.Drawing.Size(112, 20);
+            this.tex_Player4.TabIndex = 9;
+            // 
+            // tex_Player5
+            // 
+            this.tex_Player5.Location = new System.Drawing.Point(15, 416);
+            this.tex_Player5.Name = "tex_Player5";
+            this.tex_Player5.Size = new System.Drawing.Size(112, 20);
+            this.tex_Player5.TabIndex = 10;
+            // 
+            // List_Classes
+            // 
+            this.List_Classes.FormattingEnabled = true;
+            this.List_Classes.Location = new System.Drawing.Point(455, 30);
+            this.List_Classes.Name = "List_Classes";
+            this.List_Classes.Size = new System.Drawing.Size(156, 160);
+            this.List_Classes.TabIndex = 11;
+            // 
+            // Lab_Classe
+            // 
+            this.Lab_Classe.AutoSize = true;
+            this.Lab_Classe.Location = new System.Drawing.Point(452, 14);
+            this.Lab_Classe.Name = "Lab_Classe";
+            this.Lab_Classe.Size = new System.Drawing.Size(43, 13);
+            this.Lab_Classe.TabIndex = 12;
+            this.Lab_Classe.Text = "Classes";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::jogo_assassino.Properties.Resources.Voltar;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(370, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 51);
+            this.button1.TabIndex = 14;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Seguinte
+            // 
+            this.Seguinte.BackColor = System.Drawing.Color.Transparent;
+            this.Seguinte.BackgroundImage = global::jogo_assassino.Properties.Resources.Seguinte1;
+            this.Seguinte.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Seguinte.Location = new System.Drawing.Point(498, 434);
+            this.Seguinte.Name = "Seguinte";
+            this.Seguinte.Size = new System.Drawing.Size(113, 51);
+            this.Seguinte.TabIndex = 13;
+            this.Seguinte.UseVisualStyleBackColor = false;
+            this.Seguinte.Click += new System.EventHandler(this.Seguinte_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::jogo_assassino.Properties.Resources.PontoDeInterrogação;
+            this.pictureBox1.Location = new System.Drawing.Point(-13, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(641, 506);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Player1, Estás Pronto?";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 452);
+            this.ClientSize = new System.Drawing.Size(623, 493);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Seguinte);
+            this.Controls.Add(this.Lab_Classe);
+            this.Controls.Add(this.List_Classes);
+            this.Controls.Add(this.tex_Player5);
+            this.Controls.Add(this.tex_Player4);
+            this.Controls.Add(this.tex_Player3);
+            this.Controls.Add(this.tex_Player2);
+            this.Controls.Add(this.tex_Player1);
+            this.Controls.Add(this.Lab_Player5);
+            this.Controls.Add(this.Lab_Player4);
+            this.Controls.Add(this.Lab_Player3);
+            this.Controls.Add(this.Lab_Player2);
+            this.Controls.Add(this.Lab_Player1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form3";
-            this.Text = "Form3";
+            this.Text = "Ver Quem Vai Ser Quem";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Lab_Player1;
+        private System.Windows.Forms.Label Lab_Player2;
+        private System.Windows.Forms.Label Lab_Player3;
+        private System.Windows.Forms.Label Lab_Player4;
+        private System.Windows.Forms.Label Lab_Player5;
+        private System.Windows.Forms.TextBox tex_Player1;
+        private System.Windows.Forms.TextBox tex_Player2;
+        private System.Windows.Forms.TextBox tex_Player3;
+        private System.Windows.Forms.TextBox tex_Player4;
+        private System.Windows.Forms.TextBox tex_Player5;
+        private System.Windows.Forms.ListBox List_Classes;
+        private System.Windows.Forms.Label Lab_Classe;
+        private System.Windows.Forms.Button Seguinte;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- jogo_assassino/jogo_assassino/Form3.cs
+++ jogo_assassino/jogo_assassino/Form3.cs
@@ -12,9 +12,32 @@ namespace jogo_assassino
 {
     public partial class Form3 : System.Windows.Forms.Form
     {
+         = new List<>;
+
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void Seguinte_Click(object sender, EventArgs e)
+        {
+            Lab_Classe.Hide();
+            Lab_Player1.Hide();
+            Lab_Player2.Hide();
+            Lab_Player3.Hide();
+            Lab_Player4.Hide();
+            Lab_Player5.Hide();
+            List_Classes.Hide();
+            tex_Player1.Hide();
+            tex_Player2.Hide();
+            tex_Player3.Hide();
+            tex_Player4.Hide();
+            tex_Player5.Hide();
+            Seguinte.Enabled = false;
+
+           
+            }
+
         }
     }
 }
