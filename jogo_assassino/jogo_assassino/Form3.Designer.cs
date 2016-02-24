78e83d794a0b711dcf7ff12e9f37e30e96c53daf
Michael Luis <luis_michael_lol@hotmail.com>
2016-02-24 09:02:18 +0000

    Canhola

--- jogo_assassino/jogo_assassino/Form3.Designer.cs
+++ jogo_assassino/jogo_assassino/Form3.Designer.cs
@@ -35,6 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(525, 452);
+            this.ControlBox = false;
             this.Name = "Form3";
             this.Text = "Form3";
             this.ResumeLayout(false);
