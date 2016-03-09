6ab62d1764b91d79fdfc85d10e77d590530454d1
Michael Luis <luis_michael_lol@hotmail.com>
2016-03-09 09:07:43 +0000

    ok

--- jogo_assassino/jogo_assassino/Properties/Resources.Designer.cs
+++ jogo_assassino/jogo_assassino/Properties/Resources.Designer.cs
@@ -193,6 +193,16 @@ namespace jogo_assassino.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap graficos {
+            get {
+                object obj = ResourceManager.GetObject("graficos", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Inocente {
             get {
                 object obj = ResourceManager.GetObject("Inocente", resourceCulture);
