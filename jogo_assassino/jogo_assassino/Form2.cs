df0d24ac50eeeab3222c28b5a2be0588df2e8274
Nelson Silva <nelsonsilva1999@hotmail.com>
2016-03-14 11:17:00 +0000

    Tudo atualizado

--- jogo_assassino/jogo_assassino/Form2.cs
+++ jogo_assassino/jogo_assassino/Form2.cs
@@ -25,12 +25,11 @@ namespace jogo_assassino
         }
 
 
-        /*public void set_jogo(menu_jogo mn)
+        public void set_jogo(menu_jogo mn)
         {
             mn2 = mn;
         }
-
-    */
+ 
 
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
