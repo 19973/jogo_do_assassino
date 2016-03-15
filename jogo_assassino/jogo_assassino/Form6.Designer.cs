0aea6ce19d7247afbda88d3dc43182653f6ff96e
Michael Luis <luis_michael_lol@hotmail.com>
2016-03-15 14:35:37 +0000

    ok

--- jogo_assassino/jogo_assassino/Form6.Designer.cs
+++ jogo_assassino/jogo_assassino/Form6.Designer.cs
@@ -101,7 +101,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::jogo_assassino.Properties.Resources.vitoria1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.label5);
